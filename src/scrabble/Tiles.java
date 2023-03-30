a44acc936cf40df0258675d61b1f2ba97b2e6ac8
chante_graceehi@yahoo.co.uk <chante.ehigie@yahoo.co.uk>
2023-03-30 17:30:28 +0100

    I don't actually know, committing in VBS is a struggle man been trying for months

---
+++ src/scrabble/Tiles.java
@@ -0,0 +1,24 @@
+package scrabble;
+
+import java.util.HashMap;
+
+public class Tiles {
+	
+	/*char letter;
+	int value;
+	
+	public void Tiles (char letter, int value) {
+		this.letter = letter;
+		this.value = value;
+		
+	}*/
+	
+	
+	public static void main(String[] args){
+		
+		HashMap <String, Integer> tile = new HashMap <>();
+		tile.put("a", 1);
+		tile.put("b", 2);
+		
+	}
+}
